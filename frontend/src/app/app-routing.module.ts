04d125f311ce29960eee5868d5ddbc935c51aaf9
Prateek Chaplot <prateekchaplot96@gmail.com>
2023-08-06 12:04:41 +0530

    Add register UI

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { authGuard } from './auth.guard';
+import { authGuard, loginGuard } from './auth.guard';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent, canActivate: [loginGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [loginGuard] }
 ];
 
 @NgModule({
--- frontend/src/app/auth.guard.ts
+++ frontend/src/app/auth.guard.ts
@@ -1,11 +1,21 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
+const isLoggedIn = false;
+
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  const isAuthenticated = false;
-  if (!isAuthenticated) {
+  if (!isLoggedIn) {
     router.navigate(['/login'])
+  }
+
+  return true;
+};
+
+export const loginGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  if (isLoggedIn) {
+    router.navigate(['/home'])
   }
 
   return true;
