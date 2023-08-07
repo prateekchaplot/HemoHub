26e7f5a33b4990fde4442d8cf89651bc8e160a25
Prateek Chaplot <prateekchaplot96@gmail.com>
2023-08-07 19:49:36 +0530

    Fix swagger api name

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Quiz Builder API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Backend API", Version = "v1" });
 
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
